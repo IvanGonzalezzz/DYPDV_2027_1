4f7e834cbac3f25051d66b6d0b8f06419430bced
IvanGonzalezzz <ivan_gonzalez@ciencias.unam.mx>
2026-09-24 23:47:31 -0600

    paso 5 practica 2 Comunicación entre Jugador.cs y ControlJugador.cs

--- Ayudantia/Practica02/Proyecto2D/Assets/Scripts/Jugador.cs
+++ Ayudantia/Practica02/Proyecto2D/Assets/Scripts/Jugador.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 
 public class Jugador : MonoBehaviour{
-    private void Awake()
-    {
+    public bool enSuelo = false;
+    private void Awake(){
         Debug.Log("Awake: Objeto Jugador cargado en memoria");
     }
-    private void Start()
-    {
+    private void Start(){
         Debug.Log("Start: Iniciando el objeto Jugador antes del primer frame");
     }
-    private void OnCollisionEnter2D(Collision2D col)
-    {
+    private void OnCollisionEnter2D(Collision2D col){
         Debug.Log("OnCollisionEnter2D: Colision con " + col.gameObject.name);
+        if (col.gameObject.CompareTag("Suelo")){
+            enSuelo = true;
+        }
     }
-    private void OnCollisionStay2D(Collision2D col)
-    {
+    private void OnCollisionStay2D(Collision2D col){
         Debug.Log("OnCollisionStay2D: Manteniendo con " + col.gameObject.name);
+        private void OnCollisionExit2D(Collision2D col){
+            if (col.gameObject.CompareTag("Suelo")){
+                enSuelo = false;
+            }
+        }
     }
 }
